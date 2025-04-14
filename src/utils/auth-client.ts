7d37ae1c074ca89b83b64afccdfc506e6eed69d2
your username <yogeshsatkari2@gmail.com>
2025-04-15 00:37:05 +0530

    auth client url

--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -7,7 +7,7 @@ import {
 } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000", // the base url of your auth server. If the auth server is running on the same domain as the client, we can skip this line.
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "", // the base url of your auth server. If the auth server is running on the same domain as the client, we can skip this line.
   plugins: [
     oneTapClient({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
